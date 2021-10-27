a7dc30014757148f0f7d73a552dead75dccc5133
Jori Ryan <creatorcrategames@gmail.com>
2021-10-27 13:58:05 -0600

    Do you dare fight Pantsyfancy?

--- Program.cs
+++ Program.cs
@@ -4,9 +4,47 @@ namespace More_Git_Practice
 {
 	class Program
 	{
+		const string fightCommand = "fight";
+		const string fleeCommand = "flee";
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("First!");
+			RunChoice();
+		}
+
+		static void RunChoice()
+		{
+			Console.WriteLine("Pantsyfancy bites!");
+			Console.WriteLine($"Will you {fightCommand.ToUpper()} or {fleeCommand.ToUpper()}?");
+			string action = Console.ReadLine();
+
+			RunFightOrFlight(action);
+		}
+
+		static void RunFightOrFlight(string action)
+		{
+			if (action.ToLower() == fightCommand)
+			{
+				Console.WriteLine("Pantsyfancy bites your head off.");
+				Console.WriteLine("You have died.");
+				RunFinal();
+			}
+			else if (action.ToLower() == fleeCommand)
+			{
+				Console.WriteLine("You flee and never look back.");
+				RunFinal();
+			}
+			else
+			{
+				Console.WriteLine("You have only 2 options!");
+				RunChoice();
+			}
+		}
+
+		static void RunFinal()
+		{
+			Console.WriteLine("Any key to exit.");
+			Console.ReadKey();
 		}
 	}
 }
